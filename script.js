974d251c2eefc49ecbd0b9cee88124e1c50c5161
nitin-soma <113281767+nitin-soma@users.noreply.github.com>
2024-08-27 23:52:42 +0530

    chess movements added

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-// Create a 2D array to represent the chessboard
+let selectedPiece = null;
+let selectedPosition = null;
+
 let board = [];
 for (let i = 0; i < 8; i++) {
   board[i] = [];
@@ -7,49 +9,88 @@ for (let i = 0; i < 8; i++) {
   }
 }
 
-// Function to render the chessboard into HTML
 function renderBoard() {
   let html = "";
   for (let i = 0; i < 8; i++) {
     html += "<div class='row'>";
     for (let j = 0; j < 8; j++) {
       let squareClass = (i + j) % 2 === 0 ? "white" : "black";
-      html += `<div class='square ${squareClass}' id='square-${i}-${j}'>${board[i][j]}</div>`;
+      let piece = board[i][j];
+      let pieceImg = "";
+
+      if (piece) {
+        let color = piece[0] === piece[0].toUpperCase() ? "white" : "black";
+        let pieceType = piece.toLowerCase();
+
+        let pieceUrl = getPieceUrl(color, pieceType);
+        pieceImg = `<img src="${pieceUrl}" alt="${piece}" width="50px" height="50px">`;
+      }
+
+      html += `<div class='square ${squareClass}' id='square-${i}-${j}' onClick='handleClick(${i},${j})'>${pieceImg}</div>`;
     }
     html += "</div>";
   }
   document.getElementById("chessboard").innerHTML = html;
 }
 
-// Initialize the chessboard with pieces
 function initBoard() {
-  // Set up the pawns
   for (let i = 0; i < 8; i++) {
-    board[1][i] = "P";
-    board[6][i] = "p";
+    board[1][i] = "Pawn";
+    board[6][i] = "pawn";
   }
 
-  // Set up the rooks
-  board[0][0] = board[0][7] = "R";
-  board[7][0] = board[7][7] = "r";
+  board[0][0] = board[0][7] = "Rook";
+  board[7][0] = board[7][7] = "rook";
 
-  // Set up the knights
-  board[0][1] = board[0][6] = "N";
-  board[7][1] = board[7][6] = "n";
+  board[0][1] = board[0][6] = "Knight";
+  board[7][1] = board[7][6] = "knight";
 
-  // Set up the bishops
-  board[0][2] = board[0][5] = "B";
-  board[7][2] = board[7][5] = "b";
+  board[0][2] = board[0][5] = "Bishop";
+  board[7][2] = board[7][5] = "bishop";
 
-  // Set up the queens
-  board[0][3] = "Q";
-  board[7][3] = "q";
+  board[0][3] = "Queen";
+  board[7][3] = "queen";
 
-  // Set up the kings
-  board[0][4] = "K";
-  board[7][4] = "k";
+  board[0][4] = "King";
+  board[7][4] = "king";
 }
 
-// Initialize the board and render it
+function getPieceUrl(color, pieceType) {
+  const baseUrl = "https://upload.wikimedia.org/wikipedia/commons/";
+
+  const urls = {
+    "white-pawn": "4/45/Chess_plt45.svg",
+    "white-rook": "7/72/Chess_rlt45.svg",
+    "white-knight": "7/70/Chess_nlt45.svg",
+    "white-bishop": "b/b1/Chess_blt45.svg",
+    "white-queen": "1/15/Chess_qlt45.svg",
+    "white-king": "4/42/Chess_klt45.svg",
+    "black-pawn": "c/c7/Chess_pdt45.svg",
+    "black-rook": "f/ff/Chess_rdt45.svg",
+    "black-knight": "e/ef/Chess_ndt45.svg",
+    "black-bishop": "9/98/Chess_bdt45.svg",
+    "black-queen": "4/47/Chess_qdt45.svg",
+    "black-king": "f/f0/Chess_kdt45.svg",
+  };
+
+  return baseUrl + urls[`${color}-${pieceType}`];
+}
+
+function handleClick(i, j) {
+  let piece = board[i][j];
+  console.log(piece, selectedPiece);
+  if (selectedPiece) {
+    board[i][j] = selectedPiece;
+    board[selectedPosition[0]][selectedPosition[1]] = "";
+    selectedPiece = null;
+    selectedPosition = null;
+  } else if (piece) {
+    selectedPiece = piece;
+    selectedPosition = [i, j];
+  }
+
+  renderBoard();
+}
+
 initBoard();
 renderBoard();
